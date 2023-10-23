6d9bc96ce23fccddedbce81ebba6b593d64c7834
ElenaKorsakova <lenawwka@gmail.com>
2023-10-23 11:56:18 +0200

    Add: new routes, fix structure

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-require("dotenv").config();
-const jsonServer = require("json-server");
-const morgan = require("morgan");
+require('dotenv').config();
+const jsonServer = require('json-server');
+const morgan = require('morgan');
 
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT;
 
 server.use(middlewares);
-server.use(morgan("dev"));
+server.use(morgan('dev'));
 server.use((req, res, next) => {
   // Middleware to disable CORS
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 server.use(router);
